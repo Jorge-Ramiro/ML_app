8b32b459e5d285f18cbcb5813816f33ccab75407
Ramiro <ramiro2689@outlook.com>
2023-01-26 13:25:51 -0600

    Code fixes 2.0

--- building_dataset.py
+++ building_dataset.py
@@ -6,7 +6,7 @@ def dataset(path_data, batch_size=None, img_h=228, img_w=228):
     train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
         path_data,
         seed=123,
-        validation_split=0.3,
+        validation_split=0.4,
         subset="both",
         image_size=(img_h, img_w),
         batch_size=batch_size)
@@ -17,17 +17,22 @@ def dataset(path_data, batch_size=None, img_h=228, img_w=228):
     train_ds = tf.data.Dataset.zip((train_ds, (counter, counter)))
     train_ds = (train_ds
         .map(data_augmentation, num_parallel_calls=tf.data.AUTOTUNE)
-        .batch(32)
+        .batch(16)
         .prefetch(tf.data.AUTOTUNE))
-    val_ds = configure_for_performance(val_ds).batch(32)
+    val_ds = configure_for_performance(val_ds, subset="validation")
 
     return train_ds, val_ds, class_names
+
+
+def resize_img(img, label):
+    return tf.image.resize(img, [180, 180]), label
 
 
 def normalize_img(img, label):
     img = tf.cast(img, tf.float32)
     img = img / 255.0
     return img, label
+
 
 def data_augmentation(image_label, seed):
     img, label = image_label
@@ -37,15 +42,18 @@ def data_augmentation(image_label, seed):
     img = tf.image.stateless_random_crop(value=img, size=[180, 180, 3], seed=seed)
     random_angle = tf.random.stateless_uniform([], seed=new_seed, minval=0.2617993877991494, maxval=6.021385919380436, dtype=tf.dtypes.float32, alg='auto_select')
     img = tfa.image.rotate(img, angles=random_angle)
-
     return img, label
 
-def configure_for_performance(ds):
+def configure_for_performance(ds, subset="train"):
     ds = ds.map(normalize_img, num_parallel_calls=tf.data.AUTOTUNE)
-    ds = ds.shuffle(buffer_size=1000)
+    if subset == "train":
+        ds = ds.shuffle(buffer_size=1000)
+    elif subset == "validation":
+        ds = ds.map(resize_img, num_parallel_calls=tf.data.AUTOTUNE)
+        ds = ds.batch(16)
+        ds = ds.cache()
     ds = ds.prefetch(tf.data.AUTOTUNE)
     return ds
-
 
 
 if __name__ == "__main":
