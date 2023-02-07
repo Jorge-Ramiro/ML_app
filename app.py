dfda5ba7945c846a857ba4520b98cab268582aba
Ramiro <ramiro2689@outlook.com>
2023-02-06 23:20:13 -0600

    View that receives the data and sends the prediction

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ from model import Model
 
 app = Flask(__name__)
 
-model = Model()
-
 @app.route("/")
 def home():
     return render_template("main.html")
 
-
-@app.route('/test', methods=['POST'])
-def test():
-    output = request.get_json()
-    result = json.loads(output)
-    result = model.predict(result['arr'])
-    return result
-
+model = Model()
+predicted = None
+@app.route('/prediction', methods=['POST', "GET"])
+def prediction():
+    global predicted
+    if request.method == "POST":
+        c_output = request.get_json()
+        result = json.loads(c_output)
+        predicted = model.predict(result['arr'])
+        return predicted
+    else:
+        return {"breed":predicted}
 
 
 if __name__ == '__main__':
